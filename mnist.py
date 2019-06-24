07f4ac5cfa3cfb2d5eda6cb5afdf8b49c8747450
yangsen228 <ys578676728@gmail.com>
2019-06-25 08:56:41 +0900

    add some comments

--- mnist.py
+++ mnist.py
@@ -1,6 +1,7 @@
 from keras.datasets import mnist
 from keras.utils import to_categorical
 
+# Prepare training data
 train_X, train_y = mnist.load_data()[0]
 train_X = train_X.reshape(-1, 28, 28, 1)
 train_X = train_X.astype('float32')
@@ -12,6 +13,7 @@ from keras.layers import Conv2D, MaxPool2D, Flatten, Dropout, Dense
 from keras.losses import categorical_crossentropy
 from keras.optimizers import Adadelta
 
+# Build model
 model = Sequential()
 model.add(Conv2D(32, (5,5), activation='relu', input_shape=[28, 28, 1]))
 model.add(Conv2D(64, (5,5), activation='relu'))
@@ -22,16 +24,19 @@ model.add(Dense(128, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(10, activation='softmax'))
 
+# Compile model
 model.compile(loss=categorical_crossentropy,
              optimizer=Adadelta(),
              metrics=['accuracy'])
 
+# Train model
 batch_size = 100
 epochs = 8
 model.fit(train_X, train_y,
          batch_size=batch_size,
          epochs=epochs)
 
+# Evaluate model
 test_X, test_y = mnist.load_data()[1]
 test_X = test_X.reshape(-1, 28, 28, 1)
 test_X = test_X.astype('float32')
@@ -40,6 +45,7 @@ test_y = to_categorical(test_y, 10)
 loss, accuracy = model.evaluate(test_X, test_y, verbose=1)
 print('loss:%.4f accuracy:%.4f' %(loss, accuracy))
 
+# Output model
 import h5py
 from keras.models import model_from_json
 json_string = model.to_json()
--- mnist_app.py
+++ mnist_app.py
@@ -1,20 +1,20 @@
 from keras.models import model_from_json
-
-model = model_from_json(open('mnist_model_architecture.json').read())
-model.load_weights('mnist_model_weights.h5')
-
 from keras.datasets import mnist
 import cv2
 
-# test_X.shape = (10000,28,28)
-test_X, test_y = mnist.load_data()[1]
+# Load model
+model = model_from_json(open('mnist_model_architecture.json').read())
+model.load_weights('mnist_model_weights.h5')
 
+# Prepare test data
+test_X, test_y = mnist.load_data()[1] # (10000,28,28)
+
+# Predict part of the test set
 num = 999
 for i in range(20):
-	# test_X_sample.shape = (28,28)
-	test_X_sample = test_X[i,:,:]
-
+	test_X_sample = test_X[i,:,:] # (28,28)
 	test_X_sample_preds = test_X_sample.reshape(-1, 28, 28, 1)
+
 	preds = model.predict(test_X_sample_preds)
 	for i, prob in enumerate(preds[0]):
 		if prob==1:
@@ -22,4 +22,3 @@ for i in range(20):
 	cv2.putText(test_X_sample, str(num), (1,8), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255,255,255), 1)
 	cv2.imshow('test', test_X_sample)
 	cv2.waitKey(1000)
-
